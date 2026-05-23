6c84cf12d01ce2c4864b5b1b1f12df0e6f44922a
AndersonPerdom <46365836+AndersonPerdom@users.noreply.github.com>
2026-05-23 15:16:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -97,10 +97,22 @@ def descargar_excel():
             nombre, identidad = registro
             ws.append([i, nombre, identidad, ""])
 
-        for row in ws.iter_rows(min_row=3, max_row=ws.max_row, min_col=1, max_col=4):
-            for cell in row:
-                cell.border = border
-                cell.alignment = Alignment(vertical="center")
+       for row in ws.iter_rows(min_row=3, max_row=ws.max_row, min_col=1, max_col=4):
+
+    for cell in row:
+        cell.border = border
+
+        # Columna A = Número
+        if cell.column == 1:
+            cell.alignment = Alignment(
+                horizontal="left",
+                vertical="center"
+            )
+
+        else:
+            cell.alignment = Alignment(
+                vertical="center"
+            )
 
         ws.column_dimensions["A"].width = 10
         ws.column_dimensions["B"].width = 42
